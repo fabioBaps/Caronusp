a6ca9bb221c82ff0e1e1bb3a9b3f7b3fe1fe74c5
fabioBaps <fabiox2509@gmail.com>
2023-11-24 21:18:16 -0300

    procurar corridas + registro de models no admin

--- passageiro/urls.py
+++ passageiro/urls.py
@@ -7,4 +7,5 @@ app_name = 'passageiro'
 urlpatterns = [
     path('<int:usuario_id>/', views.initial, name='initial'),
     path('<int:usuario_id>/edit', views.UpdateView, name='edit'),
+    path('<int:usuario_id>/search', views.SearchView, name='search'),
 ]
--- passageiro/views.py
+++ passageiro/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from accounts.models import Usuario
+from accounts.models import Usuario, Corrida
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def initial(request, usuario_id):
-    context = {'usuario_id': usuario_id}
-    return render(request, 'passageiro/initial.html', context)
+    user = request.user
+    usuario = get_object_or_404(Usuario, pk=usuario_id)
+    if usuario.is_passageiro and usuario.id == user.id:
+        context = {'usuario': usuario}
+        return render(request, 'passageiro/initial.html', context)
+    else:
+        return HttpResponseRedirect(
+            reverse('accounts:afterlogin', args=(user.id, )))
 
 @login_required
 def UpdateView(request, usuario_id):
+    user = request.user
     usuario = get_object_or_404(Usuario, pk=usuario_id)
     if request.method == "POST":
         # usuario.foto = request.POST['foto']
@@ -23,5 +30,24 @@ def UpdateView(request, usuario_id):
         usuario.save()
         return HttpResponseRedirect(
             reverse('passageiro:initial', args=(usuario.id, )))
-    context = {'usuario': usuario}
-    return render(request, 'passageiro/edit.html', context)
+    if usuario.is_passageiro and usuario.id == user.id:
+        context = {'usuario': usuario}
+        return render(request, 'passageiro/edit.html', context)
+    else :
+        return HttpResponseRedirect(
+            reverse('accounts:afterlogin', args=(user.id, )))
+        
+@login_required
+def SearchView(request, usuario_id):
+    user = request.user
+    usuario = get_object_or_404(Usuario, pk=usuario_id)
+    if usuario.is_passageiro and usuario.id == user.id:
+        context = {}
+        if request.GET.get('query', False):
+            search_term = request.GET['query'].lower()
+            corrida_list = Corrida.objects.filter(dia__icontains=search_term)
+            context = {"corrida_list": corrida_list}
+        return render(request, 'passageiro/search.html', context)
+    else:
+        return HttpResponseRedirect(
+            reverse('accounts:afterlogin', args=(user.id, )))
