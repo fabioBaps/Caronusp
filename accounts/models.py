47feb2bca95da00f582792ef917f969faa3a53f3
fabioBaps <fabiox2509@gmail.com>
2023-11-29 21:44:26 -0300

    superuser render

--- accounts/models.py
+++ accounts/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 class UsuarioManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, username, password, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), username=username,  **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, username, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -21,7 +21,7 @@ class UsuarioManager(BaseUserManager):
         if extra_fields.get('is_superuser') is False:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, username, password, **extra_fields)
     
 class Usuario(AbstractUser):
     telefone = models.CharField(max_length=11)
